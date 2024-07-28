cb036a72fa95346555a29a4f2caea34cdd6e303a
descent <paulius@neveikia.neveikia>
2024-07-28 19:33:02 +0300

    add shadow to music select

--- Graphics/_shared header/default.lua
+++ Graphics/_shared header/default.lua
@@ -1,4 +1,12 @@
 local t = Def.ActorFrame {
+    LoadActor("shadow.png") .. {
+        InitCommand = function(self)
+            self:x(SCREEN_CENTER_X)
+            self:y(SCREEN_CENTER_Y)
+            self:zoomx(0.45)
+            self:zoomy(0.45)
+        end
+    }
 };
 
 return t;
