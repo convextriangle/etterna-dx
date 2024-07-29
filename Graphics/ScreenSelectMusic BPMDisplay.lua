9fb4bf3b579146b0799faf74ea74da68b91cfbee
descent <paulius@neveikia.neveikia>
2024-07-29 22:07:00 +0300

    Fix bpm text

--- Graphics/ScreenSelectMusic BPMDisplay.lua
+++ Graphics/ScreenSelectMusic BPMDisplay.lua
@@ -1,14 +1,14 @@
 --Code by Daisuke Master
 
 return Def.ActorFrame {
-    LoadFont("BPMDisplay")..{ Name="bpmtxt" };
-    LoadActor("_bpm label")..{ Name="bpmlbl" };
-    InitCommand=function(self)
+    LoadFont("BPMDisplay") .. { Name = "bpmtxt" },
+    LoadActor("_bpm label") .. { Name = "bpmlbl" },
+    InitCommand = function(self)
         --GetChild gets an actor so you can put commands on it and get values from it too
         self:GetChild("bpmtxt"):horizalign(right)
         self:GetChild("bpmlbl"):horizalign(left)
-    end;
-    CurrentSongChangedMessageCommand=function(self)
+    end,
+    CurrentSongChangedMessageCommand = function(self)
         local song = GAMESTATE:GetCurrentSong()
         --get out if no song
         if not song then
@@ -18,18 +18,13 @@ return Def.ActorFrame {
         --things
         local bpm = song:GetDisplayBpms()
         local text = ""
-        --this always happens
-        if bpm[1] == bpm[2] then
-            text = string.format("%d",bpm[1])
-        else
-            text = string.format("%d ~ %d",bpm[1],bpm[2])
-        end
+        text = string.format("%d", bpm[1])
         --setting the text
-		self:diffusealpha(1)
+        self:diffusealpha(1)
         self:GetChild("bpmtxt"):settext(text);
         --get the width *after* setting the text
         local width = self:GetChild("bpmtxt"):GetWidth();
         --make adjustments here
-        self:GetChild("bpmlbl"):x(-width-42)
-    end;
+        self:GetChild("bpmlbl"):x(-width - 118)
+    end,
 }
