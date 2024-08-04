7eb01bfc98b31d54f2298a5d0131e91e8901405d
descent <paulius@neveikia.neveikia>
2024-08-04 21:04:09 +0300

    fix font ini files

--- BGAnimations/ScreenTitleMenu background/default.lua
+++ BGAnimations/ScreenTitleMenu background/default.lua
@@ -1,6 +1,6 @@
 local t = Def.ActorFrame {
 	-- [Layer1]
-	LoadFont("01 48px") .. {
+	LoadFont("01/01 48px") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
 			self:settextf("Etterna...")
