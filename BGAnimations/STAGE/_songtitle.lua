d3e0556fa2f740b204d29b8a1c7be6196577b19c
descent <paulius@neveikia.neveikia>
2024-08-15 21:08:47 +0300

    add playeroptions info

--- BGAnimations/STAGE/_songtitle.lua
+++ BGAnimations/STAGE/_songtitle.lua
@@ -380,4 +380,42 @@ return Def.ActorFrame {
 			self:settext(song:GetDisplayArtist())
 		end
 	},
+
+	LoadFont("artist 16px") .. {
+		Text = "playeroptions",
+		InitCommand = function(self)
+			self:x(320):y(SCREEN_CENTER_Y + 30):playcommand("Set"):zoom(0.7):shadowlength(0):maxwidth(290)
+		end,
+		OnCommand = function(self)
+			self:finishtweening():playcommand("Set")
+		end,
+		OffCommand = function(self)
+			self:finishtweening():playcommand("Set")
+		end,
+		CurrentSongChangedMessageCommand = function(self)
+			self:finishtweening():playcommand("Set")
+		end,
+		SetCommand = function(self)
+			local song = GAMESTATE:GetCurrentSong()
+			if not song
+			then
+				self:visible(false)
+				return
+			end
+			self:visible(true)
+			self:y(SCREEN_CENTER_Y + 30)
+			self:zoom(.7)
+			self:addy(10)
+			self:diffusealpha(0)
+			self:sleep(.2)
+			self:linear(0.15)
+			self:addy(-10)
+			self:diffusealpha(1)
+			self:sleep(2.05)
+			self:decelerate(.7)
+			self:zoom(.5)
+			self:y(SCREEN_CENTER_Y + 29)
+			self:settext("Press ENTER to go to Player Options!")
+		end
+	},
 }
