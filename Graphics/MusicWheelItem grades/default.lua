c335e7c3f605f1a5a69bbeb0e09e680a2ce81d6c
descent <paulius@neveikia.neveikia>
2024-08-15 15:33:21 +0300

    tried to add difficulty text (failed)

--- Graphics/MusicWheelItem grades/default.lua
+++ Graphics/MusicWheelItem grades/default.lua
@@ -30,43 +30,62 @@ local grades = {
 	Grade_None = nil,
 };
 
-local t = LoadActor("grades") .. {
-	InitCommand = function(self)
-		self:blend('BlendMode_Add'):pause();
-		self:zoomx(284 / 800);
-		self:zoomy(28 / 60);
-		self:addx(1.5);
-	end,
-	SetGradeCommand = function(self, params)
-		local state = grades[params.Grade];
-		if state ~= nil then
-			self:visible(true);
 
-			-- no colors for all the midgrades so
-			-- just gonna use a single one for multiple
+local t = Def.ActorFrame {
+	LoadActor("grades") .. {
+		InitCommand = function(self)
+			self:blend('BlendMode_Add'):pause();
+			self:zoomx(284 / 800);
+			self:zoomy(28 / 60);
+			self:addx(1.5);
+		end,
+		SetGradeCommand = function(self, params)
+			local state = grades[params.Grade];
+			if state ~= nil then
+				self:visible(true);
 
-			if state > 0 and state <= 3 then
-				state = 1;
-			elseif state <= 6 then
-				state = 2;
-			elseif state <= 9 then
-				state = 3;
-			elseif state <= 12 then
-				state = 4;
-			elseif state <= 15 then
-				state = 5;
-			elseif state == 16 then
-				state = 6;
+				-- no colors for all the midgrades so
+				-- just gonna use a single one for multiple
+
+				if state > 0 and state <= 3 then
+					state = 1;
+				elseif state <= 6 then
+					state = 2;
+				elseif state <= 9 then
+					state = 3;
+				elseif state <= 12 then
+					state = 4;
+				elseif state <= 15 then
+					state = 5;
+				elseif state == 16 then
+					state = 6;
+				else
+					state = 7;
+				end
+
+				state = state * 2;
+				self:setstate(state);
 			else
-				state = 7;
+				self:visible(false);
 			end
+		end,
+	},
 
-			state = state * 2;
-			self:setstate(state);
-		else
-			self:visible(false);
-		end
-	end,
-};
+	-- LoadFont('diff/diffblue') .. {
+	-- 	InitCommand = function(self)
+	-- 		self:settext('')
+	-- 		self:zoom(0.4)
+	-- 		self:addx(13.5)
+	-- 	end,
+	-- 	CurrentStepsChangedMessageCommand = function(self)
+	-- 		local steps = GAMESTATE:GetCurrentSteps()
+	-- 		if steps then
+	-- 			-- displaying only overall MSD
+	-- 			local meter = steps:GetMSD(getCurRateValue(), 1);
+	-- 			self:settextf('%d', math.floor(meter))
+	-- 		end
+	-- 	end,
+	-- }
+}
 
 return t;
