1d222963191b64dcf30274c6fd15c6a129c7b020
descent <paulius@neveikia.neveikia>
2024-08-15 19:46:43 +0300

    add bg to titlescreen

--- BGAnimations/ScreenTitleMenu background/default.lua
+++ BGAnimations/ScreenTitleMenu background/default.lua
@@ -1,4 +1,14 @@
 local t = Def.ActorFrame {
+	Def.Sprite {
+		InitCommand = function(self)
+			self:LoadBackground(THEME:GetPathG("", "_selectbg.mp4"))
+			self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM)
+			self:sleep(0.25)
+			self:smooth(0.5)
+			self:diffusealpha(1)
+		end
+	},
+
 	-- [Layer1]
 	LoadFont("01/01 48px") .. {
 		InitCommand = function(self)
