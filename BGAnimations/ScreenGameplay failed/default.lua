c662ce9b720bd012a4861ca583059b1ba9c88c15
descent <paulius@neveikia.neveikia>
2024-08-16 21:51:00 +0300

    add failed to ScreenGameplay

--- BGAnimations/ScreenGameplay failed/default.lua
+++ BGAnimations/ScreenGameplay failed/default.lua
@@ -1,9 +1,13 @@
-local t = Def.ActorFrame{
-	LoadActor("failed")..{
-	InitCommand=function(self)
-		self:zoomx(SCREEN_WIDTH/640)
-	end;
-	};
+local t = Def.ActorFrame {
+	Def.Sprite {
+		InitCommand = function(self)
+			self:LoadBackground(THEME:GetPathG("", "_failed.mp4"))
+			self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM)
+			self:sleep(2)
+			self:smooth(0.5)
+			self:diffusealpha(1)
+		end
+	}
 };
 
 return t;
