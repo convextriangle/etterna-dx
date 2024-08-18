72979f2156c45749d5f0ee9ad35860b07445428f
descent <paulius@neveikia.neveikia>
2024-08-18 19:16:53 +0300

    stuffs

--- BGAnimations/ScreenEvaluation underlay/default.lua
+++ BGAnimations/ScreenEvaluation underlay/default.lua
@@ -1,62 +1,79 @@
+-- i forgot about lua argument passing rules so idk if this always works
+local function transform(self)
+	self:zoom(0.45)
+end
+
 local t = Def.ActorFrame {
 	LoadActor("epic_triangle") .. {
 		InitCommand = function(self)
-			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			self:x(SCREEN_CENTER_X):y(SCREEN_TOP + 10)
+			transform(self)
 		end
 	},
 	LoadActor("file_info") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("free_stage_result") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("game_type") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("judge_detail") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("nm") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("note_count") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("player_hp_frame") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("player_info") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("score_info") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("separator") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 	LoadActor("song_info_bar") .. {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y)
+			transform(self)
 		end
 	},
 
--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # EC:FN theme port for Etterna v0.72.3
 - todo: lots of stuffs
 
-# ???
-aoi zipped their theme with a password (donburako) for some reason (japan moment - filthy foreigner do not brain) so i should've done the same
-
-i made this for myself and my frens, if you don't like what i did - too bad :^) (but report bugs and stuff kthx)
-
 # original README
 
 ric(@Aoi_psy)
