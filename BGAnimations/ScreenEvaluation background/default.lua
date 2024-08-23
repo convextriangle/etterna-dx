744dcbb276f23e09a9a3e278404fca236fae9220
descent <paulius@neveikia.neveikia>
2024-08-23 17:25:38 +0300

    make bg a bit brighter sometimes

--- BGAnimations/ScreenEvaluation background/default.lua
+++ BGAnimations/ScreenEvaluation background/default.lua
@@ -4,12 +4,12 @@ local t = Def.ActorFrame {
             self:finishtweening()
 
             if GAMESTATE:GetCurrentSong() and GAMESTATE:GetCurrentSong():GetBackgroundPath() then
-                self:LoadBackground(GAMESTATE:GetCurrentSong():GetBackgroundPath())
+                self:LoadBackground(GAMESTATE:GetCurrentSong():GetBackgroundPath()):diffusealpha(0.5)
             else
                 self:LoadBackground(THEME:GetPathG("", "_selectbg.mp4"))
             end
 
-            self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM):diffusealpha(0.4)
+            self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM)
             self:sleep(0.5)
             self:decelerate(2)
         end
