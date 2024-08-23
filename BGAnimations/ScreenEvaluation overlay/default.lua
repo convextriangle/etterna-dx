5852ca8a754208c9151c24d37f2c598a8331d4f4
descent <paulius@neveikia.neveikia>
2024-08-23 16:52:38 +0300

    update font funnies

--- BGAnimations/ScreenEvaluation overlay/default.lua
+++ BGAnimations/ScreenEvaluation overlay/default.lua
@@ -83,43 +83,44 @@ t[#t + 1] = Def.ActorFrame {
         self:zoom(0.85)
     end,
 
-    LoadFont("artist 16px") .. {
-        InitCommand = function(self)
-            self:valign(1)
-            self:settextf("%.6f%%", score:GetWifeScore() * 100)
-        end
-    },
-    LoadFont("artist 16px") .. {
-        InitCommand = function(self)
-            self:y(30)
-            self:settextf("%5.2f > %5.2f", steps:GetMSD(getCurRateValue(), 1), score:GetSkillsetSSR("Overall"))
-        end
-    },
-    LoadFont("artist 16px") .. {
-        InitCommand = function(self)
-            self:y(5):valign(0)
-            self:settext(score:GetModifiers() .. ", Judge " .. GetTimingDifficulty())
-            self:zoom(.4)
-        end
-    },
-    LoadFont("artist 16px") .. {
-        InitCommand = function(self)
-            self:y(15):valign(0):zoom(.4)
-            self:settext(getRateString(getCurRateValue()))
-        end
-    },
-    LoadFont("artist 16px") .. {
-        InitCommand = function(self)
-            self:y(45):valign(0):zoom(.4)
+    -- LoadFont("artist 16px") .. {
+    --     InitCommand = function(self)
+    --         self:valign(1)
+    --         self:settextf("%.6f%%", score:GetWifeScore() * 100)
+    --     end
+    -- },
+    -- LoadFont("artist 16px") .. {
+    --     InitCommand = function(self)
+    --         self:y(30)
+    --         self:settextf("%5.2f > %5.2f", steps:GetMSD(getCurRateValue(), 1), score:GetSkillsetSSR("Overall"))
+    --     end
+    -- },
+    -- LoadFont("artist 16px") .. {
+    --     InitCommand = function(self)
+    --         self:y(15):valign(0):zoom(.4)
+    --         self:settext(getRateString(getCurRateValue()))
+    --     end
+    -- },
+    -- LoadFont("artist 16px") .. {
+    --     InitCommand = function(self)
+    --         self:y(45):valign(0):zoom(.4)
 
-            local str = string.format("Pack: %s", song:GetGroupName())
-            self:settext(str)
-        end
-    }
+    --         local str = string.format("Pack: %s", song:GetGroupName())
+    --         self:settext(str)
+    --     end
+    -- }
 }
 
 -- score stats
 t[#t + 1] = Def.ActorFrame {
+    LoadFont("handel/handel 24px") .. {
+        InitCommand = function(self)
+            self:xy(SCREEN_CENTER_X - 285, SCREEN_CENTER_Y - 87)
+            self:settext(score:GetModifiers() .. ", Judge " .. GetTimingDifficulty())
+            self:zoom(.3)
+        end
+    },
+
     LoadActor("grade/" .. grades[score:GetGrade()]) .. {
         InitCommand = function(self)
             self:xy(SCREEN_CENTER_X - 285, SCREEN_CENTER_Y - 120):zoom(0.5):draworder(1)
