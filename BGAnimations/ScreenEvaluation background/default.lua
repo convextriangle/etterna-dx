3b7df7b57c4360c8adf813d3d4809c248773c78b
descent <paulius@neveikia.neveikia>
2024-08-23 15:27:48 +0300

    fix eval bg brightness

--- BGAnimations/ScreenEvaluation background/default.lua
+++ BGAnimations/ScreenEvaluation background/default.lua
@@ -1,17 +1,17 @@
 local t = Def.ActorFrame {
     Def.Sprite {
         OnCommand = function(self)
+            self:finishtweening()
+
             if GAMESTATE:GetCurrentSong() and GAMESTATE:GetCurrentSong():GetBackgroundPath() then
-                self:finishtweening()
-                self:visible(true)
                 self:LoadBackground(GAMESTATE:GetCurrentSong():GetBackgroundPath())
-                self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM)
-                self:sleep(0.5)
-                self:decelerate(2)
-                self:diffusealpha(1)
             else
-                self:visible(false)
+                self:LoadBackground(THEME:GetPathG("", "_selectbg.mp4"))
             end
+
+            self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM):diffusealpha(0.4)
+            self:sleep(0.5)
+            self:decelerate(2)
         end
     }
 };
