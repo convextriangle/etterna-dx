9c8ae908eca757b5a83f0b6e45a2ce1d4c31a094
descent <paulius@neveikia.neveikia>
2024-08-27 21:40:35 +0300

    fsdfsfs

--- BGAnimations/ScreenSelectMusic overlay/tab_search/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/tab_search/default.lua
@@ -1,3 +1,22 @@
-local t = Def.ActorFrame {}
+local t = Def.ActorFrame {
+    Name = "SearchTab",
+    InitCommand = function(self)
+        self:visible(false)
+    end,
+    TabChangedMessageCommand = function(self, params)
+        self:visible(params.name == self:GetName())
+    end,
+    CurrentSongChangedMessageCommand = function(self)
+        local song = GAMESTATE:GetCurrentSong()
+        self:visible(song == nil)
+    end,
+}
+
+t[#t + 1] = LoadActor("../tabdecorations/tab_background") .. {
+    InitCommand = function(self)
+        self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
+        self:zoom(0.45)
+    end,
+}
 
 return t
